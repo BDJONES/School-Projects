c249bddc1ddafefc7e694d5f31c1d3dff3cec304
Brandon Jones <72243416+BDJONES@users.noreply.github.com>
2020-10-27 21:10:49 -0500

    Update schwifty.c

--- schwifty.c
+++ schwifty.c
@@ -3,10 +3,10 @@
 #include <ctype.h>
 #include <stdbool.h>
                                 // This program is meant to change the way a set of words is printed out, changing the order and which characters appear
-void left(char word[]);
-void right(char word[]);
-void inc(char word[]);
-void dec(char word[]);
+void left(char word[]);         // This function causes the characters in a string to shift to the left one position (such as 'hello' -> 'elloh')
+void right(char word[]);        // This function causes the characters in a string to shift to the right one position (such as 'hello' -> 'ohell')
+void inc(char word[]);          // This function causes all characters in the string to increase to the next ASCII value (such as 'a' -> 'b') 
+void dec(char word[]);          // This function causes all characters in the string to decrease to the next ASCII value (such as 'b' -> 'a')
 
 int main(int argc, char* argv[]) {
     char word[100];
@@ -84,7 +84,7 @@ void inc(char word[]) {
     for (int i = 0; i < len; ++i) {
         char check = word[i];
         if (isalpha(check)) {
-            if (check == 'Z') {
+            if (check == 'Z') { // relaps into 'A' or 'a' or '0' if the character is 'Z' or 'z' or '9' respectively 
                 check = 'A';
             }
             else if (check == 'z') {
@@ -107,7 +107,7 @@ void inc(char word[]) {
 }
 void dec(char word[]) {
     int len = strlen(word);
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i) { // Does the same as the increase function but in reverse ('A' -> 'Z' or 'a' -> 'z' or '0' -> '9')
         char check = word[i];
         if (isalpha(check)) {
             if (check == 'A') {
